87e4872cd7c770403d855748b25ea3b135e0b3bf
walid0010 <walidchekir98@gmail.com>
2021-07-24 16:26:44 +0100

    Initial Commit

---
+++ bmiAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmiAppIOS
+//
+//  Created by chekir walid on 24/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
